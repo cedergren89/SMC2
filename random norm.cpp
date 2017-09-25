c2475b859f63af02f9e2c54ba271a77d51145377
cedergren89 <31684474+cedergren89@users.noreply.github.com>
2017-09-25 16:05:40 +0100

    Update random norm.cpp

--- random norm.cpp
+++ random norm.cpp
@@ -8,14 +8,8 @@ using namespace std;
 
 MatrixXd fun_gen_noise(int nrolls, int dim, MatrixXd Mu, MatrixXd Sigma, char a)
 {
-	//generating noise
-	//default_random_engine generator;
 	random_device rd;
 	normal_distribution<double> distribution(0.0, 1.0);
-
-	//cout << "time inside" << endl;
-	//cout << time << endl;
-	//cout << time(0) << endl;
 
 	MatrixXd noise(nrolls, dim);
 	for (int i = 0; i < nrolls; ++i) {
@@ -26,8 +20,6 @@ MatrixXd fun_gen_noise(int nrolls, int dim, MatrixXd Mu, MatrixXd Sigma, char a)
 
 	//choleshy decomposition
 	MatrixXd Y(nrolls, dim);
-	//	MatrixXd Mu(dim, 1);
-	//	MatrixXd Sigma(dim, dim);
 
 	if (a == 'n')
 	{
@@ -49,4 +41,3 @@ MatrixXd fun_gen_noise(int nrolls, int dim, MatrixXd Mu, MatrixXd Sigma, char a)
 
 	return Y;
 }
-
